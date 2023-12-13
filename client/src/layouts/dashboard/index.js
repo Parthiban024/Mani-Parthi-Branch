a94cfb563e42ae34deafbd69a65be8153db76390
Manikandan M <114073486+Manikandan717@users.noreply.github.com>
2023-12-13 09:31:32 +0530

    new

--- client/src/layouts/dashboard/index.js
+++ client/src/layouts/dashboard/index.js
@@ -34,7 +34,6 @@ import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import PropTypes from "prop-types";
-// import Divider from "@mui/material/Divider";
 import Icon from "@mui/material/Icon";
 
 import cartoon from "assets/images/cartoon.png";
@@ -49,11 +48,10 @@ const UserDataUpload = (props) => {
     setDrawerOpen(true);
   };
 
-
   // Function to handle closing the drawer
   const closeDrawer = () => {
     setDrawerOpen(false);
-  }
+  };
 
   let greet;
   const date = new Date();
@@ -83,71 +81,64 @@ const UserDataUpload = (props) => {
       <DashboardLayout>
         <DashboardNavbar />
 
-      <Card lg={{ pb: "20px", height: "100%" }}>
-      <MDBox pt={3} px={2} mb={8}>
-        <MDBox
-          display="flex"
-          flexDirection="row"
-          // flexDirection="column"
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <MDTypography variant="h3" fontWeight="bold" color="info" textTransform="capitalize">
-            <div>Good {greet}<span style={{ marginLeft: '10px' }}>{name}</span></div>
-          </MDTypography>
-          <MDBox display="flex" flexDirection="row" alignItems="center" justifyContent="flex-end">
-            <MDBox color="text" mr={0.5} lineHeight={0}>
-              <Icon color="info" fontSize="large">
-                schedule
-              </Icon>
+        <Card lg={{ pb: "20px", height: "100%" }}>
+          <MDBox pt={3} px={2} mb={8}>
+            <MDBox
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <MDTypography variant="h3" fontWeight="bold" color="info" textTransform="capitalize">
+                <div>Good {greet}<span style={{ marginLeft: '10px' }}>{name}</span></div>
+              </MDTypography>
+              <MDBox display="flex" flexDirection="row" alignItems="center" justifyContent="flex-end">
+                <MDBox color="text" mr={0.5} lineHeight={0}>
+                  <Icon color="info" fontSize="large">
+                    schedule
+                  </Icon>
+                </MDBox>
+                <MDTypography color="warning" fontWeight="regular">
+                  {today.format("LT")}
+                </MDTypography>
+              </MDBox>
             </MDBox>
-            <MDTypography color="Warning" fontWeight="regular">
-              <div> {today.format("LT")}</div>
-            </MDTypography>
+            <MDBox
+              pt={3}
+              pb={2}
+              px={2}
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="space-evenly"
+            >
+              <MDBox mb={2}>
+                <MDTypography variant="caption" color="success" fontWeight="bold">
+                  <h1>Welcome to Objectways </h1>
+                </MDTypography>
+              </MDBox>
+              <MDBox
+                component="ul"
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="space-evenly"
+                p={0}
+                m={0}
+                sx={{ listStyle: "none" }}
+              >
+                <Grid container alignItems="center">
+                  <Grid item>
+                    <MDBox component="img" src={cartoon} alt="cartoon" width="100%" mt={1} />
+                  </Grid>
+                  <Grid item>
+                    {/* Additional content goes here */}
+                  </Grid>
+                </Grid>
+              </MDBox>
+            </MDBox>
           </MDBox>
-        </MDBox>
-        <MDBox
-          pt={3}
-          pb={2}
-          px={2}
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="space-evenly"
-        >
-          <MDBox mb={2}>
-            <MDTypography variant="caption" color="success" fontWeight="bold">
-              <h1>Welcome to Objectways </h1>
-            </MDTypography>
-            {/* <MDTypography variant="caption" color="success" fontWeight="bold">
-              <h2>Here whats happening in your account today</h2>
-            </MDTypography> */}
-            {/* <MDTypography component="a" href="#" type="button" color="info" fontWeight="medium">
-            <h4> Whats New</h4>
-          </MDTypography> */}
-          </MDBox>
-          <MDBox
-            component="ul"
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="space-evenly"
-            p={0}
-            m={0}
-            sx={{ listStyle: "none" }}
-          >
-            <Grid container alignItems="center">
-              <Grid item>
-                <MDBox component="img" src={cartoon} alt="cartoon" width="100%" mt={1} />
-              </Grid>
-              <Grid item>
-                {/* <MDBox component="img" src={cartoon} alt="cartoon" width="100%" mt={1} /> */}
-              </Grid>
-            </Grid>
-          </MDBox>
-        </MDBox>
-      </MDBox>
-    </Card>
+        </Card>
       </DashboardLayout>
     </div>
   );
