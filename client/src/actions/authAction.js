1a052c5f21eaf73d94d5915048318fe31f91638b
Manikandan M <114073486+Manikandan717@users.noreply.github.com>
2024-01-01 12:17:21 +0530

    new

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,12 +1,78 @@
 
+// import setAuthToken from 'utils/setAuthToken';
+// import jwt_decode from 'jwt-decode';
+// import {GET_ERRORS,SET_CURRENT_USER,USER_LOADING} from  './types';
+// import axios from 'axios';
+
+
+// export const registerUser = (userData) => dispatch =>{
+//     axios.post('user/register',userData)
+//     .then(res=> window.location = '/authentication/sign-in')
+//     .catch(err=>dispatch(
+//         {
+//             type: GET_ERRORS,
+//             payload: err.response.data
+//         }
+//     ));
+// };
+
+// export const loginUser = userData => dispatch => {
+//     axios
+//     .post('user/login', userData)
+//     .then(res=>{
+//          // Save to localStorage
+  
+//         // Set token to localStorage
+//         const { token }  = res.data;
+//         localStorage.setItem("jwtToken", token);
+//         // Set token to Auth header
+//         setAuthToken(token);
+//         // Decode token to get user data
+//         const decoded = jwt_decode(token);
+//         // Set current user
+//         dispatch(setCurrentUser(decoded));
+//     })
+//     .catch(err =>
+//         dispatch({
+//           type: GET_ERRORS,
+//           payload: err.response.data
+//         })
+//       );
+// }
+
+//  // Set logged in user
+//  export const setCurrentUser = decoded => {
+//     return {
+//       type: SET_CURRENT_USER,
+//       payload: decoded
+//     };
+//   };
+
+//   // User loading
+//   export const setUserLoading = () => {
+//     return {
+//       type: USER_LOADING
+//     };
+//   };
+
+//    // Log user out
+//    export const logoutUser = () => dispatch => {
+//     // Remove token from local storage
+//     localStorage.removeItem("jwtToken");
+//     // Remove auth header for future requests
+//     setAuthToken(false);
+//     // Set current user to empty object {} which will set isAuthenticated to false
+//     dispatch(setCurrentUser({}));
+//   };
+
 import setAuthToken from 'utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
 import {GET_ERRORS,SET_CURRENT_USER,USER_LOADING} from  './types';
 import axios from 'axios';
-
-
+ 
+ 
 export const registerUser = (userData) => dispatch =>{
-    axios.post('user/register',userData)
+    axios.post('/api/user/register',userData)
     .then(res=> window.location = '/authentication/sign-in')
     .catch(err=>dispatch(
         {
@@ -15,13 +81,13 @@ export const registerUser = (userData) => dispatch =>{
         }
     ));
 };
-
+ 
 export const loginUser = userData => dispatch => {
     axios
     .post('user/login', userData)
     .then(res=>{
          // Save to localStorage
-  
+ 
         // Set token to localStorage
         const { token }  = res.data;
         localStorage.setItem("jwtToken", token);
@@ -39,7 +105,7 @@ export const loginUser = userData => dispatch => {
         })
       );
 }
-
+ 
  // Set logged in user
  export const setCurrentUser = decoded => {
     return {
@@ -47,14 +113,14 @@ export const loginUser = userData => dispatch => {
       payload: decoded
     };
   };
-
+ 
   // User loading
   export const setUserLoading = () => {
     return {
       type: USER_LOADING
     };
   };
-
+ 
    // Log user out
    export const logoutUser = () => dispatch => {
     // Remove token from local storage
--- client/src/reducers/AuthReducer.js
+++ client/src/reducers/AuthReducer.js
@@ -1,26 +1,82 @@
-import {SET_CURRENT_USER,USER_LOADING} from 'actions/types';
-const isEmpty = require('is-empty');
+// import {SET_CURRENT_USER,USER_LOADING} from 'actions/types';
+// const isEmpty = require('is-empty');
 
-const initialState ={
-    isAuthenticated: false,
-    user:{},
-    loading: false
+// const initialState ={
+//     isAuthenticated: false,
+//     user:{},
+//     loading: false
+// };
+
+// export default function(state = initialState, action) {
+//     switch (action.type) {
+//       case SET_CURRENT_USER:
+//         return {
+//           ...state,
+//           isAuthenticated: !isEmpty(action.payload),
+//           user: action.payload
+//         };
+//       case USER_LOADING:
+//         return {
+//           ...state,
+//           loading: true
+//         };
+//       default:
+//         return state;
+//     }
+//   }
+import { SET_CURRENT_USER, USER_LOADING } from 'actions/types';
+import isEmpty from 'is-empty';
+ 
+const initialState = {
+  isAuthenticated: false,
+  user: {},
+  loading: false
 };
-
-export default function(state = initialState, action) {
-    switch (action.type) {
-      case SET_CURRENT_USER:
-        return {
-          ...state,
-          isAuthenticated: !isEmpty(action.payload),
-          user: action.payload
-        };
-      case USER_LOADING:
-        return {
-          ...state,
-          loading: true
-        };
-      default:
-        return state;
-    }
+ 
+// Give your function a name (e.g., authReducer)
+export function authReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload || {}
+      };
+    case USER_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    default:
+      return state;
   }
+}
+ 
+export default authReducer;
+// import {SET_CURRENT_USER,USER_LOADING} from 'actions/types';
+// const isEmpty = require('is-empty');
+ 
+// const initialState ={
+//     isAuthenticated: false,
+//     user:{},
+//     loading: false
+// };
+ 
+// export default function(state = initialState, action) {
+//     switch (action.type) {
+//       case SET_CURRENT_USER:
+//         return {
+//           ...state,
+//           isAuthenticated: !isEmpty(action.payload),
+//           user: action.payload
+//         };
+//       case USER_LOADING:
+//         return {
+//           ...state,
+//           loading: true
+//         };
+//       default:
+//         return state;
+//     }
+//   }
+ 
